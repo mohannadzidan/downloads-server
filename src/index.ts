cd61390506813f12f624d98b67c8c1912b607cdd
mohannadzidan <mohannad.s.zidan@gmail.com>
2025-07-08 04:16:53 +0300

    feat: better logging of unhandled rejections

--- src/index.ts
+++ src/index.ts
@@ -75,7 +75,7 @@ async function bootstrap() {
     gracefulShutdown();
   });
   process.on("unhandledRejection", (reason, promise) => {
-    logger.error("Unhandled Rejection at:", promise, "reason:", reason);
+    logger.error("Unhandled Rejection at:", { promise, reason });
     gracefulShutdown();
   });
 }
